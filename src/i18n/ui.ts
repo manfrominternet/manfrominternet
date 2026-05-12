03704dec51ec12862ff277057970e9c6646215ae
textmanfrominternet <textamanfrominternet@gmail.com>
2026-05-12 16:50:17 +0200

    final state

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -2,7 +2,11 @@ export const ui = {
   en: {
     home: "Home",
     about: "About Me",
+    about_section:
+      "I started my journey in IT back in 2018... but first, a bit about me — so you can get a sense of who I am and how I think. Since childhood, I’ve been interested in foreign languages, chess, and a healthy lifestyle. I feel that the curiosity behind it all is really a drive to learn and optimize. I’ve achieved several successes in chess, and I also manage to maintain a healthy lifestyle despite having a sedentary job. I’m an open-minded, confident, and ambitious person; I’m motivated by challenging tasks and I’m eager to take them on. I’m creative and able to work independently. I’m interested in IT — from backend, through DevOps, to frontend — and AI-related topics are familiar to me. I continuously develop my skills in all of these areas, as shown by my projects on GitHub.",
     goals: "My Goals",
+    goals_section:
+      " In life, I value precision. I focus on learning carefully selected technologies that I consider both highly valuable and forward-looking.",
     experience: "Experience",
     techstack: "Technical Skills",
     techstackSub: "Expertise & Tools",
@@ -16,6 +20,7 @@ export const ui = {
     techstack: "Umiejętności techniczne",
     techstackSub: "Ekspertyza i Narzędzia",
     welcome: "Witaj na naszej stronie!",
+    about_section: "",
   },
   de: {
     home: "Startseite",
