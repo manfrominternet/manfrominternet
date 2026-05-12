009b579aff78b95423d9056ba257fb88ad777aab
textmanfrominternet <textamanfrominternet@gmail.com>
2026-05-12 17:00:44 +0200

    configure for pblishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://manfrominternet.github.io',
   i18n: {
     defaultLocale: 'en',
+    base: '/list_motywacyjny',
     locales: ['pl', 'en', 'de', 'ru'],
     routing: {
       prefixDefaultLocale: false
